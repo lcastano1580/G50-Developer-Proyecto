73268a599cd98bd899f9ab7b64001898e1f16b7f
Miller38 <millergutierrez38@gmail.com>
2022-10-15 16:08:34 -0500

    version 1 loopback

---
+++ Loopback/proyecto_loopback/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {ProyectoLoopbackApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new ProyectoLoopbackApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: ProyectoLoopbackApplication;
+  client: Client;
+}
